e2df333bcc8b6540d01c16f985527c3b9fed6f3c
jeseop <j3s30p@gmail.com>
2026-04-20 16:32:12 +0900

    feat: 겹치는선분의길이

--- 260420_PMS_겹치는선분의길이/j3s30p_겹치는선분의길이.py
+++ 260420_PMS_겹치는선분의길이/j3s30p_겹치는선분의길이.py
@@ -1,5 +1,14 @@
 def solution(lines):
+	counter = {}
+	for start, end in lines:
+			for i in range(start, end):
+				counter[i] = counter.get(i, 0) + 1
+    
 	answer = 0
+	for value in counter.values():
+		if value >= 2:
+			answer += 1
+    
 	return answer
 
 
@@ -8,4 +17,4 @@ lines = [[[0, 1], [2, 5], [3, 9]],		# 2
 		 [[0, 5], [3, 9], [1, 10]]]		# 8
 
 for line in lines:
-	print(f'{line}:{'\n'}{solution(line)}{'\n'}')
+    print(f"{line}:\n{solution(line)}\n")
