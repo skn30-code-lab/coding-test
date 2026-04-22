ceff859e4eb0a3b25c27de7b0891c52868c62517
jeseop <j3s30p@gmail.com>
2026-04-22 11:10:29 +0900

    refactor: 3진법뒤집기

--- 260422_PMS_3진법뒤집기/jeseop_3진법뒤집기.py
+++ 260422_PMS_3진법뒤집기/jeseop_3진법뒤집기.py
@@ -1,11 +1,7 @@
 def change(num):
     if num <= 0:
         return ''
-    char = num % 3
-    num //= 3
-    return str(char) + change(num)
+    return str(num % 3) + change(num // 3)
 
 def solution(n):
     return int(change(n), 3)
-
-print(solution(45))
