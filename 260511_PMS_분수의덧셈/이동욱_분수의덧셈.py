6426e7c5dd942c3e4b1b98ef41372015d6b1b21f
이동욱 <attatae01@gmail.com>
2026-05-08 11:53:07 +0900

    fix: 분수의덧셈

--- 260511_PMS_분수의덧셈/이동욱_분수의덧셈.py
+++ 260511_PMS_분수의덧셈/이동욱_분수의덧셈.py
@@ -1,9 +1,9 @@
 def gcd(a,b,c,d):
     x=[]
-    num = d*a + b*c
-    denom = b*d
-    for i in range(1,(min(num,denom)+1)):
-        if num % i == 0 and denom % i == 0:
+    num = d*a + b*c         # 분자
+    denom = b*d             # 분모 
+    for i in range(1,(min(num,denom)+1)):       # 1 부터 분모분자 작은값까지
+        if num % i == 0 and denom % i == 0:     # 서로 나눠서 나머지가 없을때 = 공약수
             x.append(i)
     return x
         
@@ -12,4 +12,4 @@ def gcd(a,b,c,d):
 def solution(a,b,c,d):
     num = d*a + b*c
     denom = b*d
-    return [int(num/max(gcd(a,b,c,d))),int(denom/max(gcd(a,b,c,d)))]
+    return [int(num/max(gcd(a,b,c,d))),int(denom/max(gcd(a,b,c,d)))]    # max > 최대 공약수로 나눠주기
