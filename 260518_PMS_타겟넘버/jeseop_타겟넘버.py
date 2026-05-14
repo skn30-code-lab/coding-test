e1435dd7933f887f9a8a3a0fdcf8bb817ff6863e
jeseop <j3s30p@gmail.com>
2026-05-14 12:46:02 +0900

    feat: 타겟넘버

---
+++ 260518_PMS_타겟넘버/jeseop_타겟넘버.py
@@ -0,0 +1,36 @@
+class TreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+def solution(numbers, target):
+    root = TreeNode(0)
+    current_level = [root]
+    for number in numbers:
+        next_level = []
+
+        for node in current_level:
+            node.left = TreeNode(node.value + number)
+            node.right = TreeNode(node.value - number)
+
+            next_level.append(node.left)
+            next_level.append(node.right)
+        current_level = next_level
+        
+    count = 0
+    for node in current_level:
+        if node.value == target:
+            count += 1
+
+    return count
+
+
+numbers1 = [1, 1, 1, 1, 1]
+target1 = 3
+numbers2 = [4, 1, 2, 1]
+target2 = 4
+
+print(solution(numbers1, target1)) # 5
+print(solution(numbers2, target2)) # 2
+
