17a4c9ed3b507a22ef30115606a3301a9115ce9d
DOHOON KIM <kin061474@gmail.com>
2026-06-15 17:11:47 +0900

    Comment out solution function and logic
    Comment out the implementation of the solution function and related logic.

--- 260615_PMS_폰켓몬/dovan_폰켓몬.py
+++ 260615_PMS_폰켓몬/dovan_폰켓몬.py
@@ -15,17 +15,16 @@ def solution(nums):
 # import math
 
 # def solution(nums):
-
-# counts = Counter(nums)
-# # 1. 가장 높은 빈도수(최댓값) 파악
-# max_frequency = max(counts.values())
-# # 2. 최대 빈도수와 일치하는 모든 원소 추출 (리스트 컴프리헨션)
-# most_common_elements = [element for element, freq in counts.items() if freq == max_frequency]
-# # 3. 추출된 각 원소별로 N개를 뽑는 경우의 수 계산
-# N = len(nums)//2
-# for element in most_common_elements:
-#     ways_to_pick = math.comb(set(nums), N)
-#     print(f'{ways_to_pick}')
+#     counts = Counter(nums)
+#     # 1. 가장 높은 빈도수(최댓값) 파악
+#     max_frequency = max(counts.values())
+#     # 2. 최대 빈도수와 일치하는 모든 원소 추출 (리스트 컴프리헨션)
+#     most_common_elements = [element for element, freq in counts.items() if freq ==max_frequency]
+#     # 3. 추출된 각 원소별로 N개를 뽑는 경우의 수 계산
+#     N = len(nums)//2
+#     for element in most_common_elements:
+#         ways_to_pick = math.comb(len(set(nums)), N)
+#         return ways_to_pick
 
 # 시도 3(오답)
 # from itertools import permutations
