2b281aa7c62c1d1645c3c27bd3ae1c5d910e1957
정민규 <jeongmk0315@gmail.com>
2026-06-22 00:16:06 +0900

    refactor: 햄버거만들기

--- 260622_PMS_햄버거만들기/dosupdebongu_햄버거만들기.py
+++ 260622_PMS_햄버거만들기/dosupdebongu_햄버거만들기.py
@@ -10,3 +10,22 @@ def solution(ingredient):
             del basket[-4:]
 
     return answer
+
+# ==============================
+
+# def solution(ingredient):
+#     answer = 0
+#     basket = []
+    
+#     for item in ingredient:
+#         basket.append(item)
+        
+ 
+#         if basket[-4] == 1 and basket[-3] == 2 and basket[-2] == 3 and basket[-1] == 1:
+#             answer += 1
+#             basket.pop()
+#             basket.pop()
+#             basket.pop()
+#             basket.pop()
+                
+#     return answer
